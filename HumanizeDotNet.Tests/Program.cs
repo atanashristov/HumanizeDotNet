8a11d6a0e6df5048bf05b99d6ca0c0c6ccd63ef6
Atanas Hristov <atanas.hristov@hotmail.com>
2013-12-28 19:20:53 -0600

    Sort usings

--- HumanizeDotNet.Tests/Program.cs
+++ HumanizeDotNet.Tests/Program.cs
@@ -1,9 +1,4 @@
-﻿using HumanizeDotNet.Resources;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using HumanizeDotNet;
+﻿using System;
 using System.Globalization;
 using System.Threading;
 
